88598d8369f6682e15044a8d681f99316720ddbd
realtanx <hamniba8@gmail.com>
2023-08-22 15:58:17 +0800

    use React.createElement

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -5,11 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const world = React.createElement('em', null, "World");
+const title = React.createElement('h1', null, "Hello ", world, "!");
+const link = React.createElement('a', { href: "//devdocs.io" }, "Read More");
+const group = React.createElement(React.Fragment, null, title, link);
+
+root.render(group);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
